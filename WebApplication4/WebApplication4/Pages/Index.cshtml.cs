d383fa4dccee1efc52f58eb64cc372b09ac99979
Martin Teichler <martin.teichler@condato.de>
2020-03-06 13:56:40 +0100

    Initial commit

---
+++ WebApplication4/WebApplication4/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication4.Pages
+{
+    public class ArrayModel
+    {
+        public List<SelectListItem> ArrayOfStrings { get; set; } = new List<SelectListItem>();
+    }
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public ArrayModel Model { get; set; }
+
+        public void OnGet()
+        {
+            Model = new ArrayModel();
+        }
+
+        public void OnPost()
+        {
+            System.Diagnostics.Debugger.Break();
+        }
+    }
+}
---
+++ WebApplication4/WebApplication4/wwwroot/js/site.js
@@ -0,0 +1,6 @@
+﻿$('#add-item').on('click', function () {
+    debugger;
+    var value = $('#string-value').val();
+    $('#hidden-select').append(new Option(value, value));
+    $('#table-item tr:last').after('<tr><td>' + value + '</td></tr>')
+});
